edf0386ca87d22f1fce7ab7023bf413e3f65ce25
NikitaYurjevich <80631700+NikitaYurjevich@users.noreply.github.com>
2022-11-23 16:35:58 +0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,12 +3,6 @@ const windowHeight = window.screen.availHeight;
 document.body.style.setProperty('--window-height', `${windowHeight}px`);
 document.body.style.setProperty('--header-height', `${headerHeight}px`);
 
-// const containerHeight = document.getElementById('container').clientHeight;
-// const linesIcon = document.getElementById('linesIcon');
-
-// linesIcon.style.height = `${containerHeight * 1.1}px`;
-// blind.clientHeight = containerHeight;
-
 const START_ANIMATION_TIME = 400;
 const TIME_BEFORE_SLIDES = 2000;
 
@@ -24,28 +18,12 @@ const addCaret = parent => {
     caret.innerHTML = '|';
     caret.classList.add('caret');
     parent.appendChild(caret);
-    /*const caret = document.getElementById('caret')
-    switch (action) {
-        case 'show':
-            caret.style.visibility = 'visible';
-            break;
-        case 'hide':
-            caret.style.visibility = 'hidden';
-    }*/
 };
 
 const removeCaret = parent => {
     const caret = parent.querySelector('.caret');
     caret.style.display = 'none';
 }
-
-const clickAnimation = () => {
-    const sendIcon = document.getElementById('sendMessageIcon');
-    sendIcon.classList.remove('click');
-    setTimeout(() => {
-        sendIcon.classList.add('click');
-    }, 50);
-};
 
 const typeText = (el, text, speed, isLast = false) => {
     text = [...text].reverse();
@@ -58,14 +36,11 @@ const typeText = (el, text, speed, isLast = false) => {
                 addCaret(el)
             }
             clearInterval(intervalId);
-
-            // clickAnimation();
         }
     }, speed);
 };
 
 const start = () => {
-    // const typeMessageField = document.getElementById('typeMessageField');
     const sentMessagesList = document.body.getElementsByClassName('section');
     let currentIndex = 0;
 
@@ -86,20 +61,6 @@ const start = () => {
             const currentIndexClosure = currentIndex;
 
             setTimeout(() => {
-                const blindList = document.getElementsByClassName(`lines-icon__blind`);
-                for (const blind of blindList) {
-                    if (blind) {
-                        blind.classList.add('lines-icon__blind--shifted');
-                    }
-                }
-
-                const linesIconBoxList = document.getElementsByClassName('lines-icon__box');
-                for (const linesIconBox of linesIconBoxList) {
-                    if (linesIconBox) {
-                        linesIconBox.style.visibility = 'visible';
-                    }
-                }
-
                 const crossBox = document.getElementById('crossBox');
                 crossBox.classList.add('cross__box--appearance');
 
