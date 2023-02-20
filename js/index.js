d8149f9dc952033970b3081fbba0d4ed7bb6cbcc
NikitaYurjevich <80631700+NikitaYurjevich@users.noreply.github.com>
2023-02-20 23:59:58 +0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,143 @@
-const clipBtn=document.getElementById("clipBtn"),smokeSlide=document.getElementById("smokeSlide"),titleContainerNoSmoke=document.getElementById("titleNoSmoke"),titleContainerSmoke=document.getElementById("titleSmoke"),slideNoSmokeBg=document.getElementById("slideNoSmokeBg"),postBanner=document.getElementById("postBanner"),animateSunrise=()=>{let e=0;slideNoSmokeBg.classList.add("slide--no-smoke__background-shift");let t=setInterval(()=>{e>=20?(clearInterval(t),setTimeout(()=>{postBanner.classList.add("postBanner--visible")},1e3)):(slideNoSmokeBg.style.background=`radial-gradient(circle at center, #FDEB00FF 0, #0dffe7 ${30+2*e}%,#0dffe7, #0dffe7 100%) top`,e++)},70)},SMOKE_SLIDE_LEFT_X=20,SMOKE_SLIDE_RIGHT_X=20+smokeSlide.clientWidth+10,smokeSlideMiddle=(SMOKE_SLIDE_RIGHT_X-20)/2;smokeSlide.style.clip=`rect(auto, ${smokeSlideMiddle}px, auto, auto)`;let NO_SMOKE_SLIDE_OPENED=!1,SMOKE_SLIDE_OPENED=!1;const flashlight=document.getElementById("flashlight"),redSignal=document.getElementById("redSignal").getBoundingClientRect();flashlight.style.width=`${redSignal.width-10}px`,flashlight.style.left=`${redSignal.left+5}px`,flashlight.style.top=`${redSignal.bottom-flashlight.clientHeight-redSignal.height/4}px`,document.onload=()=>{postBanner.classList.remove("postBanner--visible")};const onClip=e=>{let t=e.clientX??e.touches[0].clientX;t>40&&t<SMOKE_SLIDE_RIGHT_X-10&&(clipBtn.style.left=`${t-45}px`,smokeSlide.style.clip=`rect(auto, ${t-30}px, auto, auto)`);let l=document.body.clientWidth/2,i=t>l,n=t<l;!NO_SMOKE_SLIDE_OPENED&&n&&(animateSunrise(),NO_SMOKE_SLIDE_OPENED=!0,titleContainerNoSmoke.style.opacity="1"),!SMOKE_SLIDE_OPENED&&i&&(SMOKE_SLIDE_OPENED=!0,titleContainerSmoke.style.opacity="1"),t>=1.7*l&&postBanner.classList.add("postBanner--visible")};clipBtn.addEventListener("mousedown",()=>{window.addEventListener("mousemove",onClip)}),window.addEventListener("mouseup",()=>{window.removeEventListener("mousemove",onClip)}),clipBtn.addEventListener("touchstart",()=>{window.addEventListener("touchmove",onClip)}),window.addEventListener("touchend",()=>{window.removeEventListener("touchmove",onClip)});
+//////// ОБЩЕЕ //////////
+    const formContainer = document.getElementById('formContainer');
+
+    const shiftToRight = () => {
+        const maxValue = 60;
+        let currentValue = 0;
+
+        const animationInterval = setInterval(() => {
+            if (currentValue >= maxValue) clearInterval(animationInterval);
+            formContainer.style.clipPath = `polygon(0 100%, 100% 100%, 100% ${maxValue - currentValue}%, 0 ${currentValue}%)`;
+            currentValue++;
+        }, 30);
+    }
+
+    const shiftToLeft = () => {
+        const maxValue = 60;
+        let currentValue = 0;
+
+        const animationInterval = setInterval(() => {
+            if (currentValue >= maxValue) clearInterval(animationInterval);
+            formContainer.style.clipPath = `polygon(0 100%, 100% 100%, 100% ${currentValue}%, 0 ${maxValue - currentValue}%)`;
+            currentValue++;
+        }, 30);
+    }
+/////////////////////////
+
+//////// ФОРМА 1 ////////
+    const formPhoneNumber = document.getElementById('formPhoneNumber');
+    const sendPhoneBtn = document.getElementById('sendPhoneBtn');
+    const sendPhonePreloader = document.getElementById('sendPhonePreloader');
+
+    const phoneInput = document.getElementById("phone");
+    phoneInput.addEventListener("input", function(e){
+        const reg = /(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/;
+        const x = e.target.value.replace(/\D/g, '').match(reg);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '');
+    });
+
+    formPhoneNumber.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        sendPhoneBtn.classList.add('block--hide');
+        sendPhonePreloader.classList.remove('block--hide');
+
+        const phoneNumber = phoneInput.value.replace(/\D/g, '');
+        const countryCode = document.getElementById('countryCode').value;
+        const payload = countryCode + phoneNumber;
+        const url = '';
+
+        ///// УДАЛИТЬ
+        setTimeout(() => {
+            showOTPForm();
+            sendPhoneBtn.classList.remove('block--hide');
+            sendPhonePreloader.classList.add('block--hide');
+        }, 1500)
+        /////
+
+        ///// РАСКОММЕНТИТЬ
+        // fetch(url, payload)
+        //     .then(() => {
+        //         showOTPForm();
+        //     })
+        //     .catch(() => {
+        //
+        //     })
+        //     .finally(() => {
+        //         sendPhoneBtn.classList.remove('block--hide');
+        //         sendPhonePreloader.classList.add('block--hide');
+        //     })
+        /////
+    });
+    const showOTPForm = () => {
+        formContainer.classList.add('form-container--next-form');
+        formPhoneNumber.classList.add('hide');
+        formOTP.classList.add('show');
+
+        shiftToRight();
+    };
+
+/////////////////////////
+
+//////// ФОРМА 2 ////////
+    const formOTP = document.getElementById('formOTP');
+    const menu = document.getElementById('menu');
+    const otpPreloader = document.getElementById('otpPreloader');
+
+    const showMenu = () => {
+        formContainer.classList.remove('form-container--next-form');
+        formOTP.classList.remove('show');
+        menu.classList.remove('hide');
+        setTimeout(() => {
+            formContainer.classList.add('form-container--next-form');
+            formOTP.classList.add('hide');
+            menu.classList.add('show');
+        }, 50);
+
+        [...menu.querySelectorAll('.menu__btn')].forEach(menuBtn => {
+            menuBtn.classList.add('menu__btn--appearance')
+        });
+
+        shiftToLeft();
+    }
+
+    const sendOTPHandler = (otpCode) => {
+        otpPreloader.classList.remove('block--hide');
+
+        ///// УДАЛИТЬ
+        setTimeout(() => {
+            showMenu();
+            otpPreloader.classList.add('block--hide');
+        }, 1500)
+        /////
+
+        ///// РАСКОММЕНТИТЬ
+        // const url = '';
+        // fetch(url, otpCode)
+        //     .then(() => {
+        //         showMenu();
+        //     })
+        //     .catch(() => {
+        //
+        //     })
+        //     .finally(() => {
+        //         otpPreloader.classList.add('block--hide');
+        //     })
+        /////
+    }
+
+    const addNumberToInputHandler = (inputNumber) => {
+        const getInputId = inputNumber => `otpInput${inputNumber}`;
+
+        if (inputNumber === 4) {
+            document.getElementById(getInputId('4')).disabled = true;
+            const otpCode = [...document.querySelectorAll('.input--otp')]
+                .map(el=>el.value)
+                .join('');
+            sendOTPHandler(otpCode);
+        } else {
+            const nextInputId = getInputId(inputNumber + 1);
+            document.getElementById(nextInputId).focus();
+        }
+    };
+/////////////////////////
